6d68313f3615abaf2a30f6ce252dba096ed29b73
kiboomhan <kiboomhan@gmail.com>
2021-09-08 15:33:04 +0900

    Initial commit

---
+++ src/test/java/com/example/msaconfig/MsaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
